838d101c155687261a9b8dbe1d8d7832d6d0ed2e
Diogo <diogoalexandredecodasilvapt@gmail.com>
2025-11-07 11:53:37 +0000

    Add token and tokenizer headers; update CMakeLists.txt

--- internal/tokenizer.h
+++ internal/tokenizer.h
@@ -4,5 +4,53 @@
 
 #ifndef NOVA_TOKENIZER_H
 #define NOVA_TOKENIZER_H
+#include <string>
+#include <string_view>
+
+typedef unsigned long count_t;
+
+struct Info {
+    count_t index;
+    count_t line;
+    count_t column;
+
+    count_t operator++(int) {
+        ++index;
+        ++column;
+        return index;
+    }
+
+
+};
+
+class Tokenizer {
+
+    static bool has(Info& info, const std::string_view src) {
+        return info++ < src.size();
+    }
+
+    static char peak(const Info& info, const std::string_view src, count_t offset = 1) {
+        if (info.index + offset - 1 >= static_cast<count_t>(src.size())) {
+            return '\0';
+        }
+
+        return src[info.index + offset - 1];
+    }
+
+    static void consume(Info& info, const count_t offset = 1) {
+        info.index += offset;
+        info.column += offset;
+    }
+
+public:
+
+    static auto tokenize(std::stringstream& src) {
+        std::string line;
+        while (std::getline(src, line)) {
+
+        }
+    }
+
+};
 
 #endif //NOVA_TOKENIZER_H
