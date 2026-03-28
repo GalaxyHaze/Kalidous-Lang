9a0c670afe72c0dd474dcb6519a0c9fa660e3f99
diogo <diogoalexandredecodasilvapt@gmail.com>
2026-03-28 12:12:08 +0000

    WASM support update

--- impl/main.cpp
+++ impl/main.cpp
@@ -1,6 +1,15 @@
 #include <kalidous/kalidous.hpp>
 
 
-int main(const int argc, const char **argv) {
-    return kalidous_run(argc, argv);
-}
+#ifdef KALIDOUS_WASM
+#include <iostream>
+    int main() {
+        std::cout << "Thank you for installing Kalidous, however this version is opaque for WASM rigth now\n";
+        std::cout << "Im doing my best in order to have more things to show, WASM will come soon";
+        return -1;
+    }
+#else
+    int main(int argc, const char** argv) {
+        return kalidous_run(argc, argv);
+    }
+#endif
